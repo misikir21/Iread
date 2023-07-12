5cb7ea81428e018d4f3df973c079bebf8fff624e
misikira <misikirteka@gmail.com>
2023-07-12 08:33:31 +0300

    Create 5 modules from the main js file

---
+++ modules/booklist.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/prefer-default-export */
+export const bookList = () => {
+  const content1 = ` <div class="book-list-container"><h2>All Awesome Books</h2>
+      <div class="contain">
+      </div>
+      </div>
+      `;
+  return content1;
+};
---
+++ modules/contact.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+export const contact = () => {
+  const content1 = `<div class="contact-continar">
+      <h2>Contact information</h2>
+      <div class="contact-content">
+      <p>Do you have any questions<br>
+      You can reach out to us!
+      </p>
+      <ul>
+      <li> Our e-mail<a>misikirteka@gmail.com</a></li>
+      <li>Our phone number:+251930955432 </li>
+      <li> Our address : Microverse Students</li>
+      </ul>
+      </div>
+      </div>`;
+  return content1;
+};
